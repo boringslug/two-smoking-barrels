dc9ee9f8b762ef963be2b4754e7e8d924d8c80a4
boringslug <acme133822@gmail.com>
2022-06-29 12:09:05 +0300

    changed web app
    Signed-off-by: boringslug <acme133822@gmail.com>

---
+++ src/app.js
@@ -0,0 +1,21 @@
+import './app.scss';
+
+function App() {
+  return (
+    <main className='main'>
+      <h1>Ресурс створено для розповсюдження засобів пригнічення російських свобод в мережі</h1>
+
+      <p>
+        Починаючи з початку війни росії з Україною, росія -- країна-терорист.<br></br>
+        З цього моменту наша свобода, то повертати те добро, що орки несуть нам
+      </p>
+
+      <div className='list'>
+        <a href="https://github.com/boringslug/two-smoking-barrels">Github</a>
+        <a href="files/complainer.crx">Розширення Chrome для скарг на орків в 1 клік (наразі тільки instagram)</a>
+      </div>
+    </main>
+  );
+}
+
+export default App;
