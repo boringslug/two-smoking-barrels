731105ceda255a79b74d031f483707823c217124
boringslug <acme133822@gmail.com>
2022-06-29 22:05:20 +0300

    .
    Signed-off-by: boringslug <acme133822@gmail.com>

--- src/app.js
+++ src/app.js
@@ -3,18 +3,15 @@ import './app.scss';
 function App() {
   return (
     <main className='main'>
-      <h1>Ресурс створено для розповсюдження засобів пригнічення російських свобод в мережі</h1>
-
-      <p>
-        Починаючи з початку війни росії з Україною, росія -- країна-терорист.<br></br>
-        З цього моменту наша свобода, то повертати те добро, що орки несуть нам
-      </p>
-
       <div className='list'>
-        <a href="https://github.com/boringslug/two-smoking-barrels">Github</a>
-        <div>
-          <a href="files/complainer.crx">Розширення Chrome</a>
-          для скарг на орків в 1 клік (instagram, youtube)
+        <div style={{ marginBottom: '1rem' }}>
+          <a href="https://github.com/boringslug/two-smoking-barrels">Github</a>
+        </div>
+        <div style={{ marginBottom: '1rem' }}>
+          <div className='flex' style={{ gap: '0.4rem' }}>
+            <a href="files/complainer.crx" download>Розширення Chrome</a>
+            - для скарг на орків в 1 клік (instagram, youtube)
+          </div>
         </div>
       </div>
     </main>
